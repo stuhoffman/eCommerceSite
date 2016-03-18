2fb9618d4b0bdbcf56e0f2950f3854e0772537da
Stu Hoffman <stu@hoffmanswebsite.com>
2016-03-18 12:03:46 -0700

    Added definition for the model

---
+++ eCommerce.Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        public string address1 { get; set; }
+
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+
+    }
+}
